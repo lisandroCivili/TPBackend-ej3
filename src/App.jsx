9cc0445938c53f7aa5ffdfba8ba338209b468ea4
Lisandro Civili <lisandrocivili13@gmail.com>
2024-03-24 21:10:34 -0300

    logica para editar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import Inicio from "./pages/Inicio";
 import Menu from './pages/common/Menu'
 import Footer from './pages/common/Footer'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-    <Menu/>
-    <Inicio/>
-    <Footer/>
+    <BrowserRouter>
+      <Menu/>
+        <Routes>
+          <Route path="/" element={<Inicio editando={false} titulo='Agregar nuevo color'/>}/>
+          <Route path="/editar/:id" element={<Inicio editando={true} titulo='Editar color'/>}/>
+        </Routes>
+      <Footer/>
+    </BrowserRouter>
     
-    </>
-
   );
 }
 
